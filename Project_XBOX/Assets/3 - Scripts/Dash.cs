9f4d1aa6e3b36c03097361f17f9cf9048a73ec70
CdrcMag <44393506+CdrcMag@users.noreply.github.com>
2022-03-02 14:03:30 +0100

    Update Dash.cs

--- Project_XBOX/Assets/3 - Scripts/Dash.cs
+++ Project_XBOX/Assets/3 - Scripts/Dash.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Dash : Upgrade
 {
-
+    [Header("Settings")]
     public float timeBetweenDashs = 1f;
     public float dashForce = 5f;
     public float dashTime = 0.1f;
 
+    //If the player can dash
     private bool canDash = true;
     private float cpt = 0;
 
+    //player movement script and rigidbody
     private Player_Movement pm;
     private Rigidbody2D rb;
 
@@ -24,6 +26,8 @@ public class Dash : Upgrade
 
     private void Update()
     {
+
+        //Timer to allow dash after using it
         if(!canDash)
         {
             cpt += Time.deltaTime;
@@ -34,15 +38,27 @@ public class Dash : Upgrade
             }
         }
 
-
+        
+        //If the input is pressed and the player can dash
         if(Input.GetKeyDown(KeyCode.Space) && canDash)
         {
+            //Gets the mouse position and the direction from the player
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 direction = mousePos - (Vector2)pm.transform.position;
+
+            //Normalize the vector
             direction.Normalize();
+
+            //Disallow movement
             pm.canMove = false;
+
+            //Applies the dash force
             rb.velocity = direction * dashForce;
+
+            //Removes the ability to dash
             canDash = false;
+
+            //Timer to allow movement again
             StartCoroutine(DashTempo(dashTime));
         }
     }
