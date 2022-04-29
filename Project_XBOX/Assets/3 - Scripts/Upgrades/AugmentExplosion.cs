33b5dc671a963bd309eae486e4d760c32f7eaf95
CdrcMag <44393506+CdrcMag@users.noreply.github.com>
2022-04-29 10:38:25 +0200

    Fix des totems

--- Project_XBOX/Assets/3 - Scripts/Upgrades/AugmentExplosion.cs
+++ Project_XBOX/Assets/3 - Scripts/Upgrades/AugmentExplosion.cs
@@ -21,7 +21,7 @@ public class AugmentExplosion : MonoBehaviour
     {
         if(collision.gameObject.tag == "Enemy")
         {
-            print("touché");
+            collision.transform.parent.GetComponent<Enemy>().TakeDamage(Totems.Instance.DamageOnExplosion);
         }
     }
 }
